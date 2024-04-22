abad5fb1187288de1fc15deaf70a8c9f2e285fa5
JamieH01 <Jacob__Henderson@outlook.com>
2024-04-22 15:58:05 -0400

    made telescope useful

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
     'nvim-telescope/telescope.nvim',
     config = function()
-        vim.keymap.set({"n", "v"}, "<leader>f", vim.cmd.Telescope, { desc = "Open fuzzy finder" })
+        local builtin = require("telescope.builtin")
+        vim.keymap.set({"n", "v"}, "tf", builtin.live_grep, { desc = "Open fuzzy finder" })
+        vim.keymap.set({"n", "v"}, "td", builtin.diagnostics, { desc = "View diagnostics"})
+        vim.keymap.set({"n", "v"}, "ts", builtin.lsp_document_symbols, { desc = "Symbol picker"})
     end
 }
